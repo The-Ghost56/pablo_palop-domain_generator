8162285dd0004b57a989130abdb50a8ef3a713f9
The-Ghost56 <131254680+The-Ghost56@users.noreply.github.com>
2025-02-21 18:49:28 +0000

    last commit

--- app.js
+++ app.js
@@ -6,26 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  //write your code here
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = domainGenerator();
-  })
+    document.querySelector("#btn").addEventListener("click", () => {
+        document.querySelector("#excuse").innerHTML = domainGenerator();
+    })
 };
 
 
-let pronoun = ['the', 'your']
-let adj = ['great', 'big', 'awesome']
-let color = ['red', 'blue', 'yellow']
-let object = ['door', 'window', 'table']
+const pronoun = ['the', 'your']
+const adj = ['great', 'big', 'awesome']
+const color = ['red', 'blue', 'yellow']
+const object = ['door', 'window', 'table']
 
 
-const domainGenerator = (param1,param2) => {
+const domainGenerator = () => {
     pronoun.forEach(item1 => {
         adj.forEach(item2 => {
             color.forEach(item3 => {
                 object.forEach(item4 => {
                     console.log(`${item1}${item2}${item3}${item4}.com`);
-                    return(item1+item2+item3+item4+".com")
+                    return (item1 + item2 + item3 + item4 + ".com")
 
                 });
             });
